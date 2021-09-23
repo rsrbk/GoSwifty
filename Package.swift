1bff479ec35183d02d224c7e89446f0bef2638af
Enrique Garcia <github@lemambo.com>
2021-09-23 10:50:38 +0200

    #8 Fails to build with Xcode 13
    + Fix compilation error updating swift-tools-support-core

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
-        .package(url: "https://github.com/apple/swift-tools-support-core", from: "0.1.3"),
+        .package(url: "https://github.com/apple/swift-tools-support-core", from: "0.2.3"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.2.0"),
     ],
     targets: [
